1c628091ad97b9bcb12f04cba0f96ce618ae5eb7
eesoymilk <qiyoudaoyi@gmail.com>
2023-06-20 02:55:53 +0800

    Remove file handler log

--- utils/lumberjack.py
+++ utils/lumberjack.py
@@ -1,7 +1,5 @@
-import os
 import logging
 from logging import Logger, Formatter, StreamHandler
-from logging.handlers import RotatingFileHandler
 
 
 class ColorFormatter(Formatter):
@@ -49,42 +47,21 @@ class ColorFormatter(Formatter):
 
 
 def get_lumberjack(
-    name: str,
-    file_level=logging.DEBUG,
-    console_level=logging.INFO
+    name: str, level=logging.INFO
 ) -> Logger:
-    if not os.path.exists('logs'):
-        os.makedirs('logs')
-
     logger = logging.getLogger(name)
     if not logger.propagate:
         return logger
 
     # Configure the logger
     logger.propagate = False
-    logger.setLevel(file_level)
+    logger.setLevel(level)
 
     # console handler
     ch = StreamHandler()
-    ch.setLevel(console_level)
+    ch.setLevel(level)
     ch.setFormatter(ColorFormatter())
 
-    # file handler
-    fh = RotatingFileHandler(
-        filename=f'logs/{name}.log',
-        maxBytes=32 * 1024 * 1024,  # 32 MiB
-        backupCount=5,  # Rotate through 5 files
-        encoding='utf-8',
-        mode='a'
-    )
-    fh.setLevel(file_level)
-    fh.setFormatter(Formatter(
-        '[{asctime}] [{levelname:<8}] {name} {funcName}: {message}',
-        '%Y-%m-%d %H:%M:%S',
-        style='{'
-    ))
-
     logger.addHandler(ch)
-    logger.addHandler(fh)
 
     return logger
